c847826724358b10f0b17a420aac60b5821c0dc4
amiroo <amir.m.raei@gmail.com>
2020-12-06 01:15:49 +0330

    Base of project

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Player.player import Player
 
 
 if __name__ == '__main__':
-    player = Player('amirmohammad', 12345)
-    arr = player.map
-    print(arr)
+    player1 = Player('Amirmohammad', 5000000)
+    player2 = Player("Sadra", 5000000)
+    print(player1.map)
