dea6da5a6172d9988e6dc9d4c3dc44563a871423
amiroo <amir.m.raei@gmail.com>
2020-12-06 01:00:03 +0330

    Base of project

---
+++ Player/player.py
@@ -0,0 +1,10 @@
+class Player:
+    map = [
+        ['-', '-', '-'],
+        ['-', 'C', '-'],
+        ['-', '-', '-'],
+    ]
+
+    def __init__(self, nickname, money):
+        self.nickname = nickname
+        self.money = money
---
+++ main.py
@@ -0,0 +1,7 @@
+from Player.player import Player
+
+
+if __name__ == '__main__':
+    player = Player('amirmohammad', 12345)
+    arr = player.map
+    print(arr)
