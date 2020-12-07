3b90daf034cf870cab994aa407c3a91798b405f3
amiroo <amir.m.raei@gmail.com>
2020-12-07 08:36:01 +0330

    create menu

--- Player/player.py
+++ Player/player.py
@@ -5,7 +5,14 @@ class Player:
         ['6', '7', '8'],
     ]
     troops = []
+    buildings = [
+        ['0- - - ', '1- - - ', '2- - - '],
+        ['3- - - ', '4-C- - ', '5- - - '],
+        ['6- - - ', '7- - - ', '8- - - '],
+    ]
 
     def __init__(self, nickname, money):
         self.nickname = nickname
         self.money = money
+
+
--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from random import *
 
 def menu(player):
     print('player' + str(player) + ' menu: \n'
-                                   '1.Buildings construction 2.Units Training \n'
+                                   '1.Buildings construction \n'
+                                   '2.Units Training \n'
                                    '3.Show My Map and Army \n'
                                    '4.DONE \n'
-                                   'Choose your option:...')
+                                   'Choose your option: ', end='')
+
+
+def change_player(turn):
+    if turn == 1:
+        return 2
+    elif turn == 2:
+        return 1
 
 
 if __name__ == '__main__':
@@ -18,6 +26,24 @@ if __name__ == '__main__':
     player1 = Player(nickname1, 5000000)
     player2 = Player(nickname2, 5000000)
     turn = randint(1, 2)
+    start = 0
     print("Player" + str(turn) + " you should start.")
     menu(turn)
-
+    choice = input()
+    if choice == '1':
+        for i in player1.map:
+            print(i)
+        for i in player1.map:
+            for j in i:
+                if len(j) == 3:
+                    print(j[2])
+                else:
+                    print(j)
+    elif choice == '2':
+        print("2")
+    elif choice == '3':
+        print('3')
+    elif choice == '4':
+        print('4')
+    else:
+        print('Invalid choice')
