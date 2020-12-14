6aa2b23adaecb7e67a99e991033941350b7ad3ac
amiroo <amir.m.raei@gmail.com>
2020-12-15 01:00:44 +0330

    troops class change

--- Troop/troop.py
+++ Troop/troop.py
@@ -1,19 +1,24 @@
 class Troop:
-    s = {
-        'code': 'S',
-        'HP': 10,
-        'Damage': 10,
-        'price': 100
-    }
-    t = {
-        'code': 'T',
-        'HP': 100,
-        'Damage': 100,
-        'price': 400
-    }
-    f = {
-        'code': 'F',
-        'HP': 50,
-        'Damage': 250,
-        'price': 800
-    }
+    troops = [
+        {
+            'name': 'Infantryman',
+            'code': 'S',
+            'HP': 10,
+            'Damage': 10,
+            'price': 100
+        },
+        {
+            'name': 'Abrams Tanks',
+            'code': 'T',
+            'HP': 100,
+            'Damage': 100,
+            'price': 400
+        },
+        {
+            'name': 'F22 Raptor',
+            'code': 'F',
+            'HP': 50,
+            'Damage': 250,
+            'price': 800
+        }
+    ]
