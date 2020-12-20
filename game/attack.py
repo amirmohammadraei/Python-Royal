6fbfd2ea3ce5f14c9c571198af3f710c157c0322
amiroo <amir.m.raei@gmail.com>
2020-12-21 02:23:57 +0330

    iteration attack

--- game/attack.py
+++ game/attack.py
@@ -11,4 +11,65 @@ class Attack:
         for i in self.attacker.troops:
             total_troops_damage += i['count'] * i['Damage']
 
+        if len(self.defender.map[0][0]) == 3:
+            pass
+        elif len(self.defender.map[0][0]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
 
+        if len(self.defender.map[0][1]) == 3:
+            pass
+        elif len(self.defender.map[0][1]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[0][2]) == 3:
+            pass
+        elif len(self.defender.map[0][2]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[1][2]) == 3:
+            pass
+        elif len(self.defender.map[1][2]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[2][2]) == 3:
+            pass
+        elif len(self.defender.map[2][2]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[2][1]) == 3:
+            pass
+        elif len(self.defender.map[2][1]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[2][0]) == 3:
+            pass
+        elif len(self.defender.map[2][0]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[1][0]) == 3:
+            pass
+        elif len(self.defender.map[1][0]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
+
+        if len(self.defender.map[1][1]) == 3:
+            pass
+        elif len(self.defender.map[1][1]) == 1:
+            pass
+        else:
+            print("Something went wrong.")
