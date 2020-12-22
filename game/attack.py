83f5ef8fa3b52cf2178e434d856548174a4b6a7b
amiroo <amir.m.raei@gmail.com>
2020-12-22 20:42:31 +0330

    attack

--- game/attack.py
+++ game/attack.py
@@ -15,6 +15,7 @@ class Attack:
             if j['building'] == building_code:
                 total_building_damage = j['damage']
                 building_hp = j['hp']
+        print(total_troops_damage, total_building_damage, building_hp)
         return total_troops_damage, total_building_damage, building_hp
 
     def check_attack_to(self, troops_damage, building_hp):
@@ -30,143 +31,163 @@ class Attack:
                 if i['place'] == str(place):
                     i['hp'] = 0
         else:
+            print(report)
             for i in player.buildings:
                 if i['place'] == str(place):
-                    i['hp'] -= report
+                    i['hp'] = report
 
 
     def start(self):
+        global possible
+        possible = False
         if len(self.defender.map[0][0]) == 3:
             code = self.defender.map[0][0][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
                 self.handle_building_hp(self.defender, 0, 0)
+                possible = True
             else:
                 self.handle_building_hp(self.defender, res, 0)
+                possible = False
+                return 'loose'
         elif len(self.defender.map[0][0]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong0.")
 
-        if len(self.defender.map[0][1]) == 3:
+        if len(self.defender.map[0][1]) == 3 and possible is True:
             code = self.defender.map[0][1][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
                 self.handle_building_hp(self.defender, 0, 1)
+                possible = True
             else:
                 self.handle_building_hp(self.defender, res, 1)
+                possible = False
+                return 'loose'
         elif len(self.defender.map[0][1]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong1.")
 
-        if len(self.defender.map[0][2]) == 3:
+        if len(self.defender.map[0][2]) == 3 and possible is True:
             code = self.defender.map[0][2][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 2)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 2)
+                return 'loose'
         elif len(self.defender.map[0][2]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong2.")
 
-        if len(self.defender.map[1][2]) == 3:
+        if len(self.defender.map[1][2]) == 3 and possible is True:
             code = self.defender.map[1][2][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
+            print(res)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 5)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 5)
+                return 'loose'
         elif len(self.defender.map[1][2]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong5.")
-
-        if len(self.defender.map[2][2]) == 3:
+        if len(self.defender.map[2][2]) == 3 and possible is True:
             code = self.defender.map[2][2][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 8)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 8)
+                return 'loose'
         elif len(self.defender.map[2][2]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong8.")
 
-        if len(self.defender.map[2][1]) == 3:
+        if len(self.defender.map[2][1]) == 3 and possible is True:
             code = self.defender.map[2][1][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 7)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 7)
+                return 'loose'
         elif len(self.defender.map[2][1]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong7.")
 
-        if len(self.defender.map[2][0]) == 3:
+
+        if len(self.defender.map[2][0]) == 3 and possible is True:
             code = self.defender.map[2][0][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 6)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 6)
+                return 'loose'
         elif len(self.defender.map[2][0]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong6.")
 
-        if len(self.defender.map[1][0]) == 3:
+        if len(self.defender.map[1][0]) == 3 and possible is True:
             code = self.defender.map[1][0][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = cal[1]
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
+                possible = True
                 self.handle_building_hp(self.defender, 0, 3)
             else:
+                possible = False
                 self.handle_building_hp(self.defender, res, 3)
+                return 'loose'
         elif len(self.defender.map[1][0]) == 1:
+            possible = True
             pass
-        else:
-            print("Something went wrong3.")
 
-        if len(self.defender.map[1][1]) == 3:
+
+        if len(self.defender.map[1][1]) == 3 and possible is True:
             code = self.defender.map[1][1][2]
             cal = self.calculate_damage(code)
             troops_damage = cal[0]
-            building_damage = 0
-            res = self.check_attack_to(troops_damage, building_damage)
+            building_hp = cal[2]
+            res = self.check_attack_to(troops_damage, building_hp)
             if res == 0:
                 self.handle_building_hp(self.defender, 0, 4)
+                return 'win'
             else:
                 self.handle_building_hp(self.defender, res, 4)
-        elif len(self.defender.map[1][1]) == 1:
-            pass
-        else:
-            print("Something went wrong4.")
+                return 'loose'
+
